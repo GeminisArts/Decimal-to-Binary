bf67271dde721c9ec895b3fd2780240c3d0dda5b
Alexandria Woods <53483882+GeminisArts@users.noreply.github.com>
2020-01-10 11:32:43 -0800

    Add files via upload

---
+++ Decimal to Binary.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from numpy import linalg as LA
+import itertools
+from itertools import islice
+
+
+# In[ ]:
+
+
+def DecToBin(n):
+    
+    if n<=1:
+        return str(n)
+    else:
+        return DecToBin(n//2) + DecToBin(n%2)
+
+
+# In[ ]:
+
+
+NewDec = int()
+NewDecList = []
+repeat = 1
+
+while repeat < 5:
+    while NewDec <= 20:
+        if repeat == 5:
+            break
+        NewDec = int(input("Enter an integer: "))
+        NewDec2 = list(DecToBin(NewDec))
+    
+        if NewDec > 15:
+            print("This will not be added to NewDecList, try again.\n")
+            repeat -= 1
+            print(repeat)
+        elif NewDec <= 15 and NewDec >= 0:
+            NewDecList.append(NewDec)
+            print("This will be added to NewDecList.\n")
+        elif NewDec <= 0:
+            print("This will not be added to NewDecList, try again.\n")
+            repeat -= 1
+        
+        repeat += 1
+
+
+# In[ ]:
+
+
+Results = []
+
+Input = NewDecList
+
+for i in Input:
+    Input = ('{num:04b}'.format(num=i))
+    Results.append(Input)
+
+length = [1,1,1,1]
+Results2 = iter(Results)
+Output = [list(islice(Results2, elem)) for elem in length]
+ResultList = [[int(i) for i in sub] for i in Output for sub in i]
+
+
+# In[ ]:
+
+
+ResultArray = np.array(ResultList)
+print(ResultArray)
+
+
+# In[ ]:
+
+
+ResultMatrix = np.matrix(ResultArray)
+print(ResultMatrix)
+
+
+# In[ ]:
+
+
+
+
